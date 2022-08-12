daaa4d5c3e98f6635d15d7f0a5c620c0fc31c8d1
nikosvl97 <55135553+nikosvl97@users.noreply.github.com>
2022-08-12 12:56:43 +0200

    delete

--- Script/config/iseed.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'path' => '/database/seeds',
-    'chunk_size' => 500, // Maximum number of rows per insert statement
-];
--- Script/resources/views/vendor/voyager/partials/app-footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="app-footer">
-    <div class="site-footer-right font-weight-bolder">
-        © {{date('Y')}} JustFans - <code>v{{file_get_contents(public_path('/version'))}}</code>
-    </div>
-</footer>
