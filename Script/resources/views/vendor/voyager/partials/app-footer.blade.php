29c0f9a187d9823084174043c2963c4ee9d4908b
nikosvl97 <55135553+nikosvl97@users.noreply.github.com>
2022-08-12 12:23:00 +0200

    MAIN PROJECT 4.4.0 V

---
+++ Script/resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="app-footer">
+    <div class="site-footer-right font-weight-bolder">
+        © {{date('Y')}} JustFans - <code>v{{file_get_contents(public_path('/version'))}}</code>
+    </div>
+</footer>
