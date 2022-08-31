b75c74b1dc2b3e90f23010b9425d44ed1c6dfe21
nikosvl97 <55135553+nikosvl97@users.noreply.github.com>
2022-08-31 15:14:06 +0200

    Greek language

---
+++ resources/lang/greek/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Τα στοιχεία σύνδεσης δεν ταιριάζουν σε κανέναν λογαριασμό.',
+    'throttle' => 'Πάρα πολλές προσπάθειες σύνδεσης. Προσπαθήστε ξανά σε :seconds δευτερόλεπτα.',
+
+];
