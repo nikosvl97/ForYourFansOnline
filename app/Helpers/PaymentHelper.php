8d9c081a68632b32a74ee3734bdde7294639ec6e
nikosvl97 <55135553+nikosvl97@users.noreply.github.com>
2022-09-11 15:31:08 +0200

    new icons for sepa and sofort

--- app/Helpers/PaymentHelper.php
+++ app/Helpers/PaymentHelper.php
@@ -860,7 +860,7 @@ class PaymentHelper
             }
 
             $session = \Stripe\Checkout\Session::create([
-                'payment_method_types' => ['card','sepa_debit','sofort'],
+                'payment_method_types' => ['card','sepa_debit','sofort','ideal','bancontact'],
                 'line_items' => [$stripeLineItems],
                 'locale' => 'auto',
                 'customer_email' => Auth::user()->email,
