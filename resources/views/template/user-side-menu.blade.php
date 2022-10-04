1d8ca48d2fdc3392baa76a8e9417fb7a24ef193e
nikosvl97 <55135553+nikosvl97@users.noreply.github.com>
2022-10-04 15:25:03 +0200

    user-side-menu.blade.php

--- resources/views/template/user-side-menu.blade.php
+++ resources/views/template/user-side-menu.blade.php
@@ -29,6 +29,21 @@
     </div>
 
     <ul class="list-unstyled menu-elements p-0">
+
+              <div class="menu-divider"></div>
+                      <li class="{{Route::currentRouteName() == 'my.bookmarks' ? 'active' : ''}}">
+
+                      <a class="scroll-link d-flex align-items-center" href="/anja_diergarten">
+
+                          <div class="avatar-wrapper" style ="margin-right :5px; width:10%;">
+                      <img class="avatar rounded-circle" style="width: 100%;" src="https://memberarea.anja-diergarten.de/storage/users/avatar/fb56db3652704ab49000c33e2ae1c5dd.jpg">
+                  </div>{{__('Anja Diergarten')}} 👋</a>
+                  </li>
+                      <div class="menu-divider"></div>
+
+
+
+
         @if((Auth::check() && Auth::user()->email_verified_at) || (Auth::check() && !getSetting('site.enforce_email_validation')))
             <li class="{{Route::currentRouteName() == 'profile' && (request()->route("username") == Auth::user()->username) ? 'active' : ''}}">
                 <a class="scroll-link d-flex align-items-center" href="{{route('profile',['username'=>Auth::user()->username])}}">
