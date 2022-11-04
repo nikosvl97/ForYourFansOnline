03d5cd96a4ec6d3d7d6966c9e3e804ddfb32e3e9
nikosvl97 <55135553+nikosvl97@users.noreply.github.com>
2022-11-04 20:44:06 +0100

    auth

--- resources/views/auth/modal-forms.blade.php
+++ resources/views/auth/modal-forms.blade.php
@@ -1,9 +1,11 @@
 <div class="login-section w-100">
     @include('auth.login-form',['mode'=>'ajax'])
+    @include('auth.social-login-box')
 </div>
 
 <div class="register-section w-100 d-none">
     @include('auth.register-form',['mode'=>'ajax'])
+    @include('auth.social-login-box')
 </div>
 
 <div class="forgot-section w-100 d-none">
